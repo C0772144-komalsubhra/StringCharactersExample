cf3ed47402d47b5864aaf14f3fc45adc3f3333eb
C0772144-komalsubhra <komalsubhra6768@gmail.com>
2020-02-06 19:10:13 -0500

    strings

--- StringCharactersExample/main.swift
+++ StringCharactersExample/main.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 
-print("Hello, World!")
+
+
+var str = "Heloo world 😍"
+for c in str{
+    print(c)
+}
+str.append("G")
+print(str)
+print(str.count)
+print(str.unicodeScalars.count)
+print(str.endIndex)
+print(str.startIndex)
+str.insert("T", at:str.startIndex)
+print(str)
+var name = "welcome to lambton college, toronto"
+print(name.hasPrefix("welcome"))
+print(name.hasSuffix("toronto"))
+print(name.contains("lamb"))
+
+for index in name.indices {
+    print("\(name[index]) ", terminator: "")
+}
+
+var s=""
+var l=String()
+
+print(s.isEmpty)
+print(l.isEmpty)
+
 
